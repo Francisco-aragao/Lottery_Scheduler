eb87138da7954900e02660abb63b16ede397487d
Vinicius Faria <artixsproductions@gmail.com>
2024-06-11 22:38:03 -0300

    feat: added initial user space program, not added to MAKEFILE yet

---
+++ xv6/user/ticketspam.c
@@ -0,0 +1,21 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/pstat.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2){
+        fprintf(2, "Usage: ticketspam [number of assigned tickets]\n");
+        exit(1);
+    }
+
+    int res = settickets(atoi(argv[1]));
+
+    if (res == -1)
+        fprintf(2, "Could not set tickets to process. Number of tickets cannot be smaller than 1\n");
+        exit(1)
+    else
+        for (;;;);
+        
+    exit(0);
+}
