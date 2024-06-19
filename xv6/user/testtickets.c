e9e6ad2cacd00cf2bbb8c33ca1f538de444d81c3
Vinicius Faria <artixsproductions@gmail.com>
2024-06-19 14:57:00 -0300

    fix: fixed testtickets and added to makefile

--- xv6/user/testtickets.c
+++ xv6/user/testtickets.c
@@ -4,7 +4,7 @@
 #include "kernel/pstat.h"
 #include "kernel/fcntl.h"
 
-void forkAndExec(char* numTickets, int pids[], int idx) {
+void forkAndExec(int numTickets, int pids[], int idx) {
     int pid = fork();
     if (pid < 0) {
         fprintf(2, "fork fail\n");
@@ -13,8 +13,11 @@ void forkAndExec(char* numTickets, int pids[], int idx) {
     
     if (pid == 0) { //child
         //execute ticket spam
-        char *args[3] = {"", numTickets, 0};
-        exec("ticketspam", args);
+        if (settickets(numTickets) == -1) {
+            fprintf(2, "Could not set tickets to process.\n");
+            exit(1);
+        }
+        while(1);
     } else { 
         //register child pid
         fprintf(2, "pid %d\n", pid);
@@ -31,21 +34,30 @@ int main(int argc, char *argv[]) {
     int fd;
     int pids[argc - 3];
     int idxs[argc - 3];
+    int tickets[argc - 3];
+    int runs = atoi(argv[1]);
     struct pstat pstat;
 
-    settickets(atoi(argv[2]));
+    if (settickets(atoi(argv[2])) == -1) {
+            fprintf(2, "Could not set tickets to parent process.\n");
+            exit(1);
+        }
 
     if((fd = open("./testtickets.csv", O_CREATE|O_WRONLY)) < 0){
         printf("testtickets: cannot open testtickets.csv\n");
         exit(1);
     }
 
+    for (int i = 0; i < argc - 3; i++) {
+        tickets[i] = atoi(argv[i + 3]);
+
+        if (i == argc - 4) fprintf(fd, "%dt_ticks\n", tickets[i], tickets[i]);
+        else fprintf(fd, "%dt_ticks, ", tickets[i], tickets[i]);
+    }
+
     for (int i=3; i < argc; i++) {
         //Run a ticketspam
-        forkAndExec(argv[i], pids, i - 3);
-
-        if (i == argc - 1) fprintf(fd, "pid_%d_tickets, pid_%d_ticks\n", pids[i - 3], pids[i - 3]);
-        else fprintf(fd, "pid_%d_tickets, pid_%d_ticks, ", pids[i - 3], pids[i - 3]);
+        forkAndExec(tickets[i-3], pids, i - 3);
 
         //Find pstat index
         getpinfo(&pstat);
@@ -54,29 +66,31 @@ int main(int argc, char *argv[]) {
         idxs[i-3] = idx;
     }
 
-    int prev = -1, runs = atoi(argv[1]);
+    int tick_data[runs][argc - 3];
     for (int i=0; i < runs; i++) {
-        for (;;) {
-            getpinfo(&pstat);
-            if (pstat.ticks[idxs[0]] != prev) {
-                printf("%d %d \n", pstat.ticks[idxs[0]], prev);
-                break;
-            }
-        } //Wait for update
+        printf("Reading %d\n", i);
+        sleep(100);
+        getpinfo(&pstat);
 
-        //Record to csv
-        for (int i=0; i < argc - 3; i++) {
-            int idx = idxs[i];
-            printf("Estou escrevendo!\n");
-            if (i == argc - 4) fprintf(fd, "%d, %d\n", pstat.tickets[idx], pstat.ticks[idx]);
-            else fprintf(fd, "%d, %d, ", pstat.tickets[idx], pstat.ticks[idx]);
+        //Record to matrix
+        for (int j=0; j < argc - 3; j++) {
+            tick_data[i][j] = pstat.ticks[idxs[j]];
         }
-        prev = pstat.ticks[idxs[0]];
     }
-    
-    close(fd); //close file
+
+    //Kill children.
     for (int i = 0; i < argc - 3; i++) {
         kill(pids[i]);
     }
+
+    //Record data to csv
+    for (int i=0; i < runs; i++) {
+        for (int j=0; j < argc - 3; j++) {
+            if (j == argc - 4) fprintf(fd, "%d\n", tick_data[i][j]);
+            else fprintf(fd, "%d,", tick_data[i][j]);
+        }
+    }
+    
+    close(fd); //close file
     exit(0);
 }
